d4e1577f77879c82d339f136c138b71f378ab6f8
yasuhiro-tsuji-rist <yasuhiro.tsuji@rist.co.jp>
2024-05-19 16:21:01 +0900

    second commit

---
+++ set_env/set_env.sh
@@ -0,0 +1,26 @@
+# ldconfig
+
+# python -m pip install --upgrade pip
+
+# # pip install -r set_env/requirements.txt
+
+# pip install albumentations==1.4.0
+# pip install lightning==2.2.1
+# pip install librosa==0.10.1
+# pip install pytorch-lightning==2.2.1
+# pip install opencv-python==4.8.0.74
+
+
+ldconfig
+
+# torchaudio.
+# git clone https://github.com/pytorch/audio
+# cd audio
+# git checkout tags/v2.0.1
+# python setup.py develop
+# cd ../
+
+# other packages.
+pip install -r set_env/requirements.txt
+
+export PYTHONPATH=./
