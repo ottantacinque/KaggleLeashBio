a60996b0fae5899aad1521ee5e8915bf36415a2c
yasuhiro-tsuji-rist <yasuhiro.tsuji@rist.co.jp>
2024-05-27 00:12:43 +0000

    desc

--- notebooks/funcs/calc_descriptor.py
+++ notebooks/funcs/calc_descriptor.py
@@ -6,6 +6,7 @@ from rdkit.ML.Descriptors import MoleculeDescriptors
 from mordred import Calculator, descriptors 
 from rdkit.Avalon.pyAvalonTools import GetAvalonFP
 from rdkit.Chem.Fingerprints import FingerprintMols
+import gc
 
 def calc_rdkit_descriptors(bb_dicts:dict):
 
@@ -80,23 +81,41 @@ def calc_maccs_descriptors(bb_dicts:dict):
     return df_maccs
 
 
-def calc_mordred_descriptors(bb_dicts:dict, ignore_3D:bool=False):
 
-    idx_list = bb_dicts.keys()
+def calc_mordred_descriptors(bb_dicts: dict, ignore_3D: bool = False, batch_size: int = 100):
+    idx_list = list(bb_dicts.keys())
     smiles_list = [bb_dicts[idx] for idx in idx_list]
     
-    mols_list = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
-    mols_list = [Chem.AddHs(mol) for mol in mols_list]
+    # 分子を小さなバッチに分割
+    def batch(iterable, n=1):
+        l = len(iterable)
+        for ndx in range(0, l, n):
+            yield iterable[ndx:min(ndx + n, l)]
 
-    mols_list_opt = []
-    for mol in mols_list:
-        AllChem.EmbedMolecule(mol, AllChem.ETKDG())
-        mols_list_opt.append(mol)
+    desc = Calculator(descriptors, ignore_3D=ignore_3D)
+    
+    # 空のデータフレームを初期化
+    df_mord_total = None
+    
+    for idx_batch, smiles_batch in zip(batch(idx_list, batch_size), batch(smiles_list, batch_size)):
+        mols_list = [Chem.MolFromSmiles(smiles) for smiles in smiles_batch]
+        mols_list = [Chem.AddHs(mol) for mol in mols_list]
+        
+        mols_list_opt = []
+        for mol in mols_list:
+            AllChem.EmbedMolecule(mol, AllChem.ETKDG())
+            mols_list_opt.append(mol)
+        
+        df_mord_batch = desc.pandas(mols_list_opt, quiet=False)
+        df_mord_batch.index = idx_batch
+        
+        if df_mord_total is None:
+            df_mord_total = df_mord_batch
+        else:
+            df_mord_total = pd.concat([df_mord_total, df_mord_batch])
+        
+        # メモリを解放
+        del mols_list, mols_list_opt, df_mord_batch
+        gc.collect()
 
-    desc = Calculator(descriptors, ignore_3D=ignore_3D) #3D記述子
-    df_mord = desc.pandas(mols_list_opt, quiet=False)
-    df_mord.index = idx_list
-    
-    del mols_list, mols_list_opt
-    
-    return df_mord
+    return df_mord_total
